01f6d1ed5d0b767dccad1d83897c5be67ec61351
Alexitomartinez62 <99413691+Alexitomartinez62@users.noreply.github.com>
2022-10-18 19:48:17 +0200

    Add files via upload

--- EjemploPráctica/src/basicoDinamico/ListController.java
+++ EjemploPráctica/src/basicoDinamico/ListController.java
@@ -19,15 +19,12 @@ public class ListController {
 
     @FXML
     private ChoiceBox<String> choice1;
-
-    @FXML
-    private ListView<String> list1;
     
-    @FXML
-    private ComboBox<String> combo1;
+
     
     @FXML
     private TableView<Person> table1;
+    
     
     @FXML
     private TableColumn<Person, String> firstNameCol;
@@ -41,8 +38,6 @@ public class ListController {
     @FXML
     private TableColumn<Person, Integer> ageColumn;
     
-    @FXML
-    private TreeView<String> tree1;
     
      // Lista auxiliar para TableView
     private ObservableList<Person> data = FXCollections.observableArrayList(
@@ -57,18 +52,11 @@ public class ListController {
     private void initialize() {   
         // Controles de JavaFX a los que se añaden directamente los items 
     	// Ítems del ChoiceBox
-        choice1.getItems().addAll("Choice1", "Choice2", "Choice3", "Choice4", "Choice5", 
-        		"Choice6", "Choice7", "Choice8", "Choice9");      
+        choice1.getItems().addAll("1 Billete", "2 Billetes", "3 Billetes", "4 Billetes", "5 Personas", 
+        		"6 Personas");      
         
-        // Ítems del ComboBox
-        combo1.getItems().addAll("Combo1", "Combo2", "Combo3", "Combo4", "Combo5", "Combo6",
-        		"Combo7", "Combo8", "Combo9");
+       
                 
-        // Ítems del ListView (con la segunda línea se asigna un CellFactory para permitir que sean editables)
-        list1.getItems().addAll("Lista1", "Lista2", "Lista3");
-        list1.setCellFactory(TextFieldListCell.forListView());
-        // Para seleccionar más de un ítem a la vez
-        list1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         
         // Asociamos cada columna del TableView a una propiedad de la clase Person 
         firstNameCol.setCellValueFactory(new PropertyValueFactory<Person,String>("firstName"));
@@ -78,31 +66,8 @@ public class ListController {
         // Se rellena la tabla con objetos de la clase Person
         table1.setItems(data);     
         
-        // Ítems para el TreeView
-        // Ítem raíz
-        TreeItem<String> rootItem = new TreeItem<String>("Tutorials");
-
-        // Ítem de primer nivel
-        TreeItem<String> webItem = new TreeItem<String>("Web Tutorials");
-        webItem.getChildren().add(new TreeItem<String>("HTML  Tutorial"));
-        webItem.getChildren().add(new TreeItem<String>("HTML5 Tutorial"));
-        webItem.getChildren().add(new TreeItem<String>("CSS Tutorial"));
-        webItem.getChildren().add(new TreeItem<String>("SVG Tutorial"));
-        rootItem.getChildren().add(webItem);
-        
-        // Otro ítem de primer nivel
-        TreeItem<String> javaItem = new TreeItem<String>("Java Tutorials");
-        javaItem.getChildren().add(new TreeItem<String>("Java Language"));
-        javaItem.getChildren().add(new TreeItem<String>("Java Collections"));
-        javaItem.getChildren().add(new TreeItem<String>("Java Concurrency"));
-        rootItem.getChildren().add(javaItem);
-        
-        // Para que sea editable necesitamos especificar un CellFactory con el tipo que corresponda
-        tree1.setCellFactory(TextFieldTreeCell.forTreeView());
-        
-        // Expadimos por defecto el ítem raíz
-        rootItem.setExpanded(true); 
-        tree1.setRoot(rootItem);               
+       
+          
     }
     
 }

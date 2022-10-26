bf422c0c4887136cd61f8367c50b33d1998e4c55
Alexitomartinez62 <99413691+Alexitomartinez62@users.noreply.github.com>
2022-10-26 16:34:52 +0200

    Add files via upload

---
+++ Aleex/src/application/MenuController.java
@@ -0,0 +1,157 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+public class MenuController {
+	
+	// Pantalla principal en la que se añade o quita contenido
+	private BorderPane rootLayout;
+  
+    @FXML
+    private void initialize() {
+        
+    }
+    
+    @FXML
+    private Button botonSiguiente;
+    
+    
+    @FXML
+    private void abrirFormulario(ActionEvent event) {    	
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlesDinamicos.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void abrirFormulario2(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlesDinamico2.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
+    @FXML
+    void abrirFormulario3(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlesDinamico3.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
+    @FXML
+    void abrirFormulario4(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlesDinamico4.fxml"));
+			BorderPane listadoControles = (BorderPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
+    @FXML
+    void abrirFormulario5(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlesPrecios.fxml"));
+			GridPane listadoControles = (GridPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void abrirAyuda(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/ControlAyuda.fxml"));
+			GridPane listadoControles = (GridPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    
+    @FXML
+    void abrirFormulario6(ActionEvent event) {
+    	try {
+			// Cargamos el archivo Controles Dinámicos
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MenuController.class.getResource("/basicoDinamico/Sample.fxml"));
+			AnchorPane listadoControles = (AnchorPane) loader.load();
+
+			// Se sitúa en el centro del diseño principal
+			rootLayout.setCenter(listadoControles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+   
+
+    
+
+    
+    
+    @FXML
+    private void cerrarListado(ActionEvent event) {    	
+    	// Se elimina el contenido del nodo central
+    	rootLayout.setCenter(null);	
+    }
+
+	public BorderPane getRootLayout() {
+		return rootLayout;
+	}
+
+	public void setRootLayout(BorderPane rootLayout) {
+		this.rootLayout = rootLayout;
+	}	
+    
+}
---
+++ Aleex/src/basicoDinamico/ListControllerEdit.java
@@ -0,0 +1,86 @@
+package basicoDinamico;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ListControllerEdit implements Initializable {
+
+    @FXML
+    private TableView<Animal> animals;
+
+    @FXML
+    private TableColumn<Animal, Integer> idCol;
+
+    @FXML
+    private TableColumn<Animal, String> typeCol;
+
+    @FXML
+    private TableColumn<Animal, String> nameCol;
+
+
+    @FXML
+    private TextField inputId;
+
+    @FXML
+    private TextField inputType;
+
+    @FXML
+    private TextField inputName;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        idCol.setCellValueFactory(new PropertyValueFactory<Animal, Integer>("id"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<Animal, String>("type"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<Animal, String>("name"));
+        setupTable();
+    }
+
+    @FXML
+    void submit(ActionEvent event) {
+        ObservableList<Animal> currentTableData = animals.getItems();
+        int currentAnimalId = Integer.parseInt(inputId.getText());
+
+        for (Animal animal : currentTableData) {
+            if(animal.getId() == currentAnimalId) {
+                animal.setType(inputType.getText());
+                animal.setName(inputName.getText());
+
+                animals.setItems(currentTableData);
+                animals.refresh();
+                break;
+            }
+        }
+    }
+
+    @FXML
+    void rowClicked(MouseEvent event) {
+        Animal clickedAnimal = animals.getSelectionModel().getSelectedItem();
+        inputId.setText(String.valueOf(clickedAnimal.getId()));
+        inputType.setText(String.valueOf(clickedAnimal.getType()));
+        inputName.setText(String.valueOf(clickedAnimal.getName()));
+    }
+
+    private void setupTable(){
+        Animal animal0 = new Animal(0,"Perro","Alejandro Martinez");
+        Animal animal1 = new Animal(1,"Gato","Fracisco León");
+        Animal animal2 = new Animal(2,"Ratón","Bob Esponja");
+        Animal animal3 = new Animal(3,"Gato","Laila Rodriguez");
+        Animal animal4 = new Animal(4,"Hámster","Wang");
+        Animal animal5 = new Animal(5,"Perro","Cristiano Ronaldo");
+        Animal animal6 = new Animal(6,"Lobo","Javier Delgado");
+        Animal animal7 = new Animal(7,"Perro","Sara Reinolds");
+        Animal animal8 = new Animal(8,"Calamar","Sergio González");
+        animals.getItems().addAll(animal0,animal1,animal2,animal3,animal4,animal5,animal6,animal7,animal8);
+    }
+}
